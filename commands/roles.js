38534fa2c4da284ca4adecff99a99a2112db3be6
Vesturo <37129099+vesturo@users.noreply.github.com>
2021-10-21 23:59:12 +0200

    automated role setup completed

--- commands/roles.js
+++ commands/roles.js
@@ -16,27 +16,29 @@ module.exports = {
      */
 
      run: async ({ guild,client }) => {
-        guild.roles.create({ name: 'Radiant', color: 'TOMATO', });
-        guild.roles.create({ name: 'Immortal 3', color: 'RED', });
-        guild.roles.create({ name: 'Immortal 2', color: 'RED', });
-        guild.roles.create({ name: 'Immortal 1', color: 'RED', });
-        guild.roles.create({ name: 'Diamond 3', color: 'AQUA', });
-        guild.roles.create({ name: 'Diamond 2', color: 'AQUA', });
-        guild.roles.create({ name: 'Diamond 1', color: 'AQUA', });
-        guild.roles.create({ name: 'Platinum 3', color: 'STEELBLUE', });
-        guild.roles.create({ name: 'Platinum 2', color: 'STEELBLUE', });
-        guild.roles.create({ name: 'Platinum 1', color: 'STEELBLUE', });
-        guild.roles.create({ name: 'Gold 3', color: 'GOLD', });
-        guild.roles.create({ name: 'Gold 2', color: 'GOLD', });
-        guild.roles.create({ name: 'Gold 1', color: 'GOLD', });
-        guild.roles.create({ name: 'Silver 3', color: 'FULL_WHITE', });
-        guild.roles.create({ name: 'Silver 2', color: 'FULL_WHITE', });
-        guild.roles.create({ name: 'Silver 1', color: 'FULL_WHITE', });
-        guild.roles.create({ name: 'Bronce 3', color: 'CHOCOLATE', });
-        guild.roles.create({ name: 'Bronce 2', color: 'CHOCOLATE', });
-        guild.roles.create({ name: 'Bronce 1', color: 'CHOCOLATE', });
-        guild.roles.create({ name: 'Iron 3', color: 'PAPAYAWHIP', });
-        guild.roles.create({ name: 'Iron 2', color: 'PAPAYAWHIP', });
-        guild.roles.create({ name: 'Iron 1', color: 'PAPAYAWHIP', });
+        guild.roles.create({ name: 'Radiant', color: '#fefeb1', });
+        guild.roles.create({ name: 'Immortal 3', color: '#be334d', });
+        guild.roles.create({ name: 'Immortal 2', color: '#be334d', });
+        guild.roles.create({ name: 'Immortal 1', color: '#be334d', });
+        guild.roles.create({ name: 'Diamond 3', color: '#c587f4', });
+        guild.roles.create({ name: 'Diamond 2', color: '#c587f4', });
+        guild.roles.create({ name: 'Diamond 1', color: '#c587f4', });
+        guild.roles.create({ name: 'Platinum 3', color: '#52d1df', });
+        guild.roles.create({ name: 'Platinum 2', color: '#52d1df', });
+        guild.roles.create({ name: 'Platinum 1', color: '#52d1df', });
+        guild.roles.create({ name: 'Gold 3', color: '#d98d20', });
+        guild.roles.create({ name: 'Gold 2', color: '#d98d20', });
+        guild.roles.create({ name: 'Gold 1', color: '#d98d20', });
+        guild.roles.create({ name: 'Silver 3', color: '#c6cdcb', });
+        guild.roles.create({ name: 'Silver 2', color: '#c6cdcb', });
+        guild.roles.create({ name: 'Silver 1', color: '#c6cdcb', });
+        guild.roles.create({ name: 'Bronce 3', color: '#865801', });
+        guild.roles.create({ name: 'Bronce 2', color: '#865801', });
+        guild.roles.create({ name: 'Bronce 1', color: '#865801', });
+        guild.roles.create({ name: 'Iron 3', color: '#5e5e5e', });
+        guild.roles.create({ name: 'Iron 2', color: '#5e5e5e', });
+        guild.roles.create({ name: 'Iron 1', color: '#5e5e5e', });
+        guild.roles.create({ name: 'Unranked', color: '#5f9695', });
+        
     }
 }
