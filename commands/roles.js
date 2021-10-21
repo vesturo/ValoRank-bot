b829c8881357db909e1941782013d209a819b5f0
Vesturo <37129099+vesturo@users.noreply.github.com>
2021-10-22 00:15:59 +0200

    stuff

--- commands/roles.js
+++ commands/roles.js
@@ -15,30 +15,44 @@ module.exports = {
      * @param {Discord.Client} param0.client  
      */
 
-     run: async ({ guild,client }) => {
-        guild.roles.create({ name: 'Radiant', color: '#fefeb1', });
-        guild.roles.create({ name: 'Immortal 3', color: '#be334d', });
-        guild.roles.create({ name: 'Immortal 2', color: '#be334d', });
-        guild.roles.create({ name: 'Immortal 1', color: '#be334d', });
-        guild.roles.create({ name: 'Diamond 3', color: '#c587f4', });
-        guild.roles.create({ name: 'Diamond 2', color: '#c587f4', });
-        guild.roles.create({ name: 'Diamond 1', color: '#c587f4', });
-        guild.roles.create({ name: 'Platinum 3', color: '#52d1df', });
-        guild.roles.create({ name: 'Platinum 2', color: '#52d1df', });
-        guild.roles.create({ name: 'Platinum 1', color: '#52d1df', });
-        guild.roles.create({ name: 'Gold 3', color: '#d98d20', });
-        guild.roles.create({ name: 'Gold 2', color: '#d98d20', });
-        guild.roles.create({ name: 'Gold 1', color: '#d98d20', });
-        guild.roles.create({ name: 'Silver 3', color: '#c6cdcb', });
-        guild.roles.create({ name: 'Silver 2', color: '#c6cdcb', });
-        guild.roles.create({ name: 'Silver 1', color: '#c6cdcb', });
-        guild.roles.create({ name: 'Bronce 3', color: '#865801', });
-        guild.roles.create({ name: 'Bronce 2', color: '#865801', });
-        guild.roles.create({ name: 'Bronce 1', color: '#865801', });
-        guild.roles.create({ name: 'Iron 3', color: '#5e5e5e', });
-        guild.roles.create({ name: 'Iron 2', color: '#5e5e5e', });
-        guild.roles.create({ name: 'Iron 1', color: '#5e5e5e', });
-        guild.roles.create({ name: 'Unranked', color: '#5f9695', });
-        
+     run: async ({ guild,client,message }) => {
+        guild.roles.create({ name: 'Radiant', color: '#fefeb1', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Immortal 3', color: '#be334d', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Immortal 2', color: '#be334d', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Immortal 1', color: '#be334d', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Diamond 3', color: '#c587f4', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Diamond 2', color: '#c587f4', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Diamond 1', color: '#c587f4', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Platinum 3', color: '#52d1df', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Platinum 2', color: '#52d1df', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Platinum 1', color: '#52d1df', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Gold 3', color: '#d98d20', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Gold 2', color: '#d98d20', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Gold 1', color: '#d98d20', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Silver 3', color: '#c6cdcb', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Silver 2', color: '#c6cdcb', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Silver 1', color: '#c6cdcb', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Bronze 3', color: '#865801', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Bronze 2', color: '#865801', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Bronze 1', color: '#865801', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Iron 3', color: '#5e5e5e', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Iron 2', color: '#5e5e5e', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Iron 1', color: '#5e5e5e', hoist: 'true', mentionable: 'false' });
+        guild.roles.create({ name: 'Unranked', color: '#5f9695', hoist: 'true', mentionable: 'false' });
+
+        let embed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('ValoRank Sync Setup')
+                .setThumbnail('https://cdn.discordapp.com/attachments/847852754047467550/900678550196207646/Vesturo_1.png')
+                .addFields(
+                    {name: 'Rollen eingerichtet', value: 'Rollen erfolgreich erstellt!\nDu Kannst diese nachricht einfach löschen, alles erledigt' }
+                )
+                .setTimestamp()
+                .setFooter('Made by Studio 5', 'https://cdn.discordapp.com/attachments/847852754047467550/900678550196207646/Vesturo_1.png');
+
+                message.channel.send({ 
+                    embeds: [embed]
+                });
+
     }
 }
